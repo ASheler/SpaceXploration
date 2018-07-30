e930ff90f3acd6f591babb335204e1aad90270d4
Sheler <ondraglaser@gmail.com>
2018-07-30 22:00:00 +0200

    add LaunchDetailActivity with Links support

--- app/src/main/java/com/glaserproject/spacexploration/LaunchDetailActivity.java
+++ app/src/main/java/com/glaserproject/spacexploration/LaunchDetailActivity.java
@@ -1,16 +1,24 @@
 package com.glaserproject.spacexploration;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.glaserproject.spacexploration.AppConstants.ExtrasKeys;
 import com.glaserproject.spacexploration.LaunchObjects.Launch;
+import com.glaserproject.spacexploration.RvAdapters.DetailLinksAdapter;
+import com.glaserproject.spacexploration.Utils.DateUtils;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -24,12 +32,16 @@ import java.util.Locale;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LaunchDetailActivity extends AppCompatActivity {
+public class LaunchDetailActivity extends AppCompatActivity implements DetailLinksAdapter.onClickHandler{
 
     private Launch launch;
 
     //Firebase Analytics
     private FirebaseAnalytics mFirebaseAnalytics;
+
+    DetailLinksAdapter linksAdapter;
+
+
 
     @BindView(R.id.launch_name)
     TextView launchTitle;
@@ -39,6 +51,10 @@ public class LaunchDetailActivity extends AppCompatActivity {
     TextView launchSite;
     @BindView(R.id.launch_rocket)
     TextView launchRocket;
+    @BindView(R.id.launch_detail)
+    TextView launchDetail;
+    @BindView(R.id.detail_links_rv)
+    RecyclerView linksRv;
 
     @BindView(R.id.adView)
     AdView adView;
@@ -82,12 +98,20 @@ public class LaunchDetailActivity extends AppCompatActivity {
 
     private void setUpUI() {
         launchTitle.setText(launch.getMission_name());
-        Date date = new Date(launch.getLaunch_date_unix()*1000L);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        String formattedDate = sdf.format(date);
+        String formattedDate = DateUtils.formateDate(launch.getLaunch_date_unix());
         launchDate.setText(formattedDate);
         launchSite.setText(launch.getLaunch_site().getSite_name_long());
         launchRocket.setText(launch.getRocket().getRocket_name());
+        launchDetail.setText(launch.getDetails());
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        linksRv.setLayoutManager(layoutManager);
+        linksAdapter = new DetailLinksAdapter(launch.getLinks(), this);
+        linksRv.setAdapter(linksAdapter);
+
+
     }
 
 
@@ -107,4 +131,13 @@ public class LaunchDetailActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onClick(String url) {
+        //send user to chrome custom tab on click on link
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        //setup brand color for toolbar
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
 }
---
+++ app/src/main/java/com/glaserproject/spacexploration/RvAdapters/DetailLinksAdapter.java
@@ -0,0 +1,218 @@
+package com.glaserproject.spacexploration.RvAdapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.glaserproject.spacexploration.LaunchObjects.Links;
+import com.glaserproject.spacexploration.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailLinksAdapter extends RecyclerView.Adapter<DetailLinksAdapter.LinksViewHolder> {
+
+    private Links links;
+    private onClickHandler clickHandler;
+
+
+    public DetailLinksAdapter(Links links, onClickHandler clickHandler) {
+        this.links = links;
+        this.clickHandler = clickHandler;
+    }
+
+
+    @NonNull
+    @Override
+    public DetailLinksAdapter.LinksViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.detail_links_tile, parent, false);
+        return new LinksViewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailLinksAdapter.LinksViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (links == null){
+            return 0;
+        }
+        return 9;
+    }
+
+    public class LinksViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @BindView(R.id.link_icon)
+        ImageView linkIcon;
+        @BindView(R.id.link_description)
+        TextView linkDesc;
+
+
+        public LinksViewHolder(View itemView) {
+            super(itemView);
+
+            ButterKnife.bind(this, itemView);
+
+
+        }
+
+
+        void bind(int position){
+            if (getLink(position) != null){
+                linkIcon.setImageResource(getResourceId(position));
+                itemView.setOnClickListener(this);
+            } else {
+                linkIcon.setImageResource(getShadowResourceId(position));
+            }
+            linkIcon.setContentDescription(getIconDesc(position));
+            linkDesc.setText(getIconDesc(position));
+        }
+
+
+
+        @Override
+        public void onClick(View v) {
+            clickHandler.onClick(getLink(getAdapterPosition()));
+        }
+    }
+
+    public String getLink(int position){
+
+        switch (position){
+            case 0:
+                return links.getMission_patch();
+            case 1:
+                return links.getMission_patch_small();
+            case 2:
+                return links.getReddit_campaign();
+            case 3:
+                return links.getReddit_launch();
+            case 4:
+                return links.getReddit_recovery();
+            case 5:
+                return links.getReddit_media();
+            case 6:
+                return links.getPresskit();
+            case 7:
+                return links.getArticle_link();
+            case 8:
+                return links.getWikipedia();
+            case 9:
+                return links.getVideo_link();
+        }
+        return null;
+
+    }
+
+    private int getResourceId(int position){
+
+        switch (position){
+            case 0:
+                return R.drawable.ic_mission_patch;
+            case 1:
+                return R.drawable.ic_mission_patch;
+            case 2:
+                return R.drawable.ic_reddit;
+            case 3:
+                return R.drawable.ic_reddit;
+            case 4:
+                return R.drawable.ic_reddit;
+            case 5:
+                return R.drawable.ic_reddit;
+            case 6:
+                return R.drawable.ic_presskit;
+            case 7:
+                return R.drawable.ic_article;
+            case 8:
+                return R.drawable.ic_wikipedia;
+            case 9:
+                return R.drawable.ic_youtube;
+        }
+        return 0;
+
+    }
+
+
+    private String getIconDesc(int position){
+
+        switch (position){
+            case 0:
+                return "Mission Patch";
+            case 1:
+                return "Mission Patch Small";
+            case 2:
+                return "Campaign thread";
+            case 3:
+                return "Launch thread";
+            case 4:
+                return "Recovery thread";
+            case 5:
+                return "Media thread";
+            case 6:
+                return "Presskit";
+            case 7:
+                return "Article";
+            case 8:
+                return "Wikipedia";
+            case 9:
+                return "Video";
+        }
+        return null;
+
+    }
+
+
+    public int getShadowResourceId(int position){
+
+        switch (position){
+            case 0:
+                return R.drawable.ic_mission_patch_gray;
+            case 1:
+                return R.drawable.ic_mission_patch_gray;
+
+            case 2:
+                return R.drawable.ic_reddit_gray;
+            case 3:
+                return R.drawable.ic_reddit_gray;
+
+            case 4:
+                return R.drawable.ic_reddit_gray;
+
+            case 5:
+                return R.drawable.ic_reddit_gray;
+
+            case 6:
+                return R.drawable.ic_article_gray;
+
+            case 7:
+                return R.drawable.ic_article_gray;
+
+            case 8:
+                return R.drawable.ic_wikipedia_gray;
+
+            case 9:
+                return R.drawable.ic_youtube_gray;
+
+        }
+        return 0;
+
+    }
+
+
+
+    //interface for the click
+    public interface onClickHandler {
+        void onClick(String url);
+    }
+
+}
